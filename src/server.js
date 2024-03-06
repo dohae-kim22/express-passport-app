01f61ff895cf1c5998a6a12b2a8131f8b8a3b86e
dohae-kim22 <dohae.kim22@gmail.com>
2024-03-06 15:37:05 +0100

    Create middleware for auth

--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const User = require("./models/users.model");
 const passport = require("passport");
 const cookieSession = require("cookie-session");
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+} = require("./middleware/auth");
 
 const app = express();
 const port = 4000;
@@ -12,6 +16,7 @@ const cookieEncryptionKey = "super-secret-key";
 
 app.use(
   cookieSession({
+    name: "cookie-session",
     keys: [cookieEncryptionKey],
   })
 );
@@ -54,11 +59,11 @@ mongoose
     console.log(err);
   });
 
-app.get("/", (req, res) => {
+app.get("/", checkAuthenticated, (req, res) => {
   res.render("index");
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login");
 });
 
@@ -77,7 +82,7 @@ app.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
-app.get("/signup", (req, res) => {
+app.get("/signup", checkNotAuthenticated, (req, res) => {
   res.render("signup");
 });
 
